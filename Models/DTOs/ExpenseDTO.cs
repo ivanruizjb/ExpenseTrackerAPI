034c3de1b875874da1f10fff1b2895e9b1c792f1
Jairo Ivan Beltran ruiz <jairoibruiz@gmail.com>
2026-05-18 20:53:40 -0500

    Primera version API ExpenseTracker

---
+++ Models/DTOs/ExpenseDTO.cs
@@ -0,0 +1,10 @@
+namespace ExpenseTracker.Models.DTOs;
+
+public class ExpenseDTO
+{
+    public string Description { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public string PaymentMethod { get; set; } = string.Empty;
+}
